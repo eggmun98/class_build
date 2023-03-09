3fe410a5137958550484e5f4e9ae373d4b286bb6
“eggmun98” <eggmun98@gmail.com>
2023-03-09 10:55:37 +0900

    다시 분기배포

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -11,8 +11,10 @@ export default function BoardsPage() {
   );
 }
 
-export const getServerSideProps = () => {
-  // 만약 서버사이드 렌더링을 하는 페이지라면? out 폴더로 생성 불가
-  // 즉 yarn build:ssg 명령어를 입력하면 오류 생김
-  // 즉 out 폴더 생성 불가
-};
+// export const getServerSideProps = () => {
+//   // 만약 서버사이드 렌더링을 하는 페이지라면? out 폴더로 생성 불가
+//   // 즉 yarn build:ssg 명령어를 입력하면 오류 생김
+//   // 즉 out 폴더 생성 불가
+
+//   // 그래서 어떻게 해야하는가?? 이런 경우는 next.config.js에서 exportPathMap 으로 현재 페이지 제외시키기
+// };
