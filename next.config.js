2019c8a19ed4368a1d3cb0dc96ff105e83ec58fe
“eggmun98” <eggmun98@gmail.com>
2023-03-09 10:54:05 +0900

    분기배포

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp_deploy-project",
+
+  // 아래 주소들만 out 폴더에 만들어줘!! (getServerSideProps 있는 페이지는 제외)
+  // 즉 아래에 있는 주소들만 빌드가 됨
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -10,3 +10,9 @@ export default function BoardsPage() {
     </>
   );
 }
+
+export const getServerSideProps = () => {
+  // 만약 서버사이드 렌더링을 하는 페이지라면? out 폴더로 생성 불가
+  // 즉 yarn build:ssg 명령어를 입력하면 오류 생김
+  // 즉 out 폴더 생성 불가
+};
